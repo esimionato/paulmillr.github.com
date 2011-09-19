572f29478bc2490b90c1ab7d576098799aff43c7
Paul Miller <paulmillr@me.com>
2011-09-19 06:07:01 +0300

    Added HTML5shiv.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,24 @@
-/* Author: 
+(function() {
+  /* Author: Paul Miller
+  */
 
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  var sidebar, _ref;
+  if ((_ref = window.useSidebar) == null) window.useSidebar = false;
+  sidebar = {
+    init: function() {
+      var $navlist;
+      $navlist = $(".navigation-list");
+      return $(".tag-bar > h2[id]").each(function() {
+        var $item, $this, id, text;
+        $this = $(this);
+        text = $this.text();
+        id = $this.attr("id");
+        $item = $("<li><a href='#" + id + "'>" + text + "</a></li>");
+        return $navlist.append($item);
+      });
+    }
+  };
+  $(function() {
+    if (window.config.useSidebar) return sidebar.init();
+  });
+}).call(this);
